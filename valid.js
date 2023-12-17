891af986be7e9499cac715318acf599840cac8df
Elyas-Rasta <130361975+Elyas-Rasta@users.noreply.github.com>
2023-12-17 11:25:09 +0000

    Update valid.js

--- valid.js
+++ valid.js
@@ -20,6 +20,30 @@ function validate(){
 
     }
     else{
-        alert ("Login Failed has to be more than 10 letters.")
+        alert ("Login Failed, Password Has To Be More Than 10.")
+    }
+}function myfunction(){
+    var x=document.getElementById("pass");
+
+    if(x.type === "password"){
+        x.type = "text";
+    }
+    else{
+        x.type = "password";
+    }
+}
+
+function validate(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length")
+
+    if(password.value.length >= 10){
+        alert("Login Succesful");
+        window.location.replace("newpage.html")
+        return false;
+
+    }
+    else{
+        alert ("Login Failed, Password Has To Be More Than 10 Letters.")
     }
 }
